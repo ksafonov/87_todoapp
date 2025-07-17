5db8260554eaddd597abdeec0d9907cab5385b26
Kirill Safonov <kirill.safonov@gmail.com>
2025-07-17 08:19:03 +0100

    allow all hosts

--- 87_todoapp/settings.py
+++ 87_todoapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'not very secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
